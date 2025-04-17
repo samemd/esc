088751e591684cb9415a117d41c161b8133aba2b
Samuel Emde <samuel.emde@gmail.com>
2025-04-17 18:22:21 +0200

    feat: fix magic link redirect; add logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,14 @@ import "~/styles/globals.css";
 import type { Metadata } from "next";
 import { Geist, Shadows_Into_Light } from "next/font/google";
 
+import { LogOut } from "lucide-react";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import type { ReactNode } from "react";
 import { Navigation } from "~/components/navigation";
+import { Button } from "~/components/ui/button";
 import { Toaster } from "~/components/ui/sonner";
+import logout from "~/server/actions/logout";
 import { auth } from "~/server/auth";
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -47,10 +50,16 @@ export default async function RootLayout({
 					<SessionProvider session={session}>
 						<Navigation />
 						<div className="pt-0 pb-16 md:pt-16 md:pb-0">
-							<div className="min-h-(--min-height) w-full bg-gradient-to-b from-black to-[#15162c]">
+							<div className="relative min-h-(--min-height) w-full bg-gradient-to-b from-black to-[#15162c]">
+								<form action={logout} className="absolute right-10 bottom-10">
+									<Button>
+										<LogOut />
+									</Button>
+								</form>
 								{children}
 							</div>
 						</div>
+
 						<Toaster
 							theme="dark"
 							position="bottom-right"
--- src/server/actions/login.ts
+++ src/server/actions/login.ts
@@ -4,14 +4,12 @@ import { signIn } from "~/server/auth";
 
 export async function googleLogin(formData: FormData) {
 	const redirectTo = formData.get("redirectTo")?.toString() || "/";
-	console.log("redirectTo", redirectTo);
-	await signIn("google", { redirectTo: "/bets" });
+	await signIn("google", { redirectTo });
 }
 
 export async function resendLogin(formData: FormData) {
 	const redirectTo = formData.get("redirectTo")?.toString() || "/";
-	console.log("redirectTo", redirectTo);
 	const email = formData.get("email");
 
-	await signIn("resend", { email }, { redirectTo: "/bets" });
+	await signIn("resend", { email, redirectTo });
 }
